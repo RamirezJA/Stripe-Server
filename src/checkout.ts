b27eac067c753a09183f5ff44eba7ec6c37c2adf
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-08-23 17:50:29 -0700

    intents billing custmrs subs

--- src/checkout.ts
+++ src/checkout.ts
@@ -7,6 +7,16 @@ import Stripe from 'stripe';
 export async function createStripeCheckoutSession(
   line_items: Stripe.Checkout.SessionCreateParams.LineItem[]
 ) {
+  // Example Item
+  // {
+  //   name: 'T-shirt',
+  //   description: 'Comfortable cotton t-shirt',
+  //   images: ['https://example.com/t-shirt.png'],
+  //   amount: 500,
+  //   currency: 'usd',
+  //   quantity: 1,
+  // }
+
   const url = process.env.WEBAPP_URL;
 
   const session = await stripe.checkout.sessions.create({
